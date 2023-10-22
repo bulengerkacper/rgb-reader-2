f3b8151dc39ae902bcdc09b3cf5a20f739867c33
bulengerkacper <bulenger.kacper@gmail.cmo>
2023-10-22 20:18:46 +0200

    clean up

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,14 @@ pub fn Calc(cx: Scope) -> Element {
                     let rgb_data = RgbData::default();
                     let files = file_engine.files();
                     for file_name in &files {
+                        filenames.write().clear();
                         filenames.write().push(file_name.to_string());
                         let rgbs = rgb_data.count_avgs(&file_name);
-                            grey.set(rgb_data.to_grey(rgbs.0,rgbs.1,rgbs.2));
+                        grey.set(rgb_data.to_grey(rgbs.0,rgbs.1,rgbs.2));
 
-                            r.set(rgbs.0);
-                            g.set(rgbs.1);
-                            b.set(rgbs.2);
+                        r.set(rgbs.0);
+                        g.set(rgbs.1);
+                        b.set(rgbs.2);
                     }
                 }
             }
